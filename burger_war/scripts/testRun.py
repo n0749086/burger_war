2da588b3288b84c5019e006cff5398887882e59c
n0749086 <n0749086@gmail.com>
2020-02-16 17:53:49 +0900

    おやや、センサー情報の可視化を始めたようですよ。
    - サンプルから派生したスクリプトを追加
    - カメラ, LIDAR, 戦況のSubscriberを追加
    - カメラ画像を別ウィンドウに追加するように設定

---
+++ burger_war/scripts/testRun.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+This is rumdom run node.
+subscribe No topcs.
+Publish 'cmd_vel' topic. 
+mainly use for simple sample program
+
+by Takuya Yamaguhi.
+'''
+
+import rospy
+import random
+
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+import cv2
+import json
+
+bVizCam = False
+
+class RandomBot():
+    def __init__(self, bot_name="NoName", showCamData=False):
+        # bot name 
+        self.name = bot_name
+        # velocity publisher
+        self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
+
+        # camera subscribver
+        # please uncoment out if you use camera
+        # for convert image topic to opencv obj
+        self.img = None
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber('image_raw', Image, self.imageCallback)
+        bVizCam = showCamData
+        rospy.loginfo(bVizCam)
+
+        # lidar scan subscriber
+        self.scan = LaserScan()
+        self.lidar_sub = rospy.Subscriber('scan', LaserScan, self.lidarCallback)
+
+	# war status
+	self.war_state = rospy.Subscriber("war_state", String, self.stateCallback)
+
+    # lidar scan topic call back sample
+    # update lidar scan state
+    def lidarCallback(self, data):
+        self.scan = data
+        #rospy.loginfo(self.scan)
+
+   # camera image call back sample
+    # comvert image topic to opencv object and show
+    def imageCallback(self, data):
+        try:
+            self.img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            rospy.logerr(e)
+
+        
+        cv2.imshow("Image window", self.img)
+        cv2.waitKey(1)
+
+
+    def calcTwist(self):
+        value = random.randint(1,1000)
+        if value < 250:
+            x = 0.2
+            th = 0
+        elif value < 500:
+            x = -0.2
+            th = 0
+        elif value < 750:
+            x = 0
+            th = 1
+        elif value < 1000:
+            x = 0
+            th = -1
+        else:
+            x = 0
+            th = 0
+        twist = Twist()
+        twist.linear.x = x; twist.linear.y = 0; twist.linear.z = 0
+        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th
+        return twist
+
+    def stateCallback(self, state):
+        dic = json.loads(state.data)
+        #rospy.loginfo(dic)
+        rospy.loginfo(("score", dic["scores"]["r"]))
+
+    def strategy(self):
+        r = rospy.Rate(1) # change speed 1fps
+
+        target_speed = 0
+        target_turn = 0
+        control_speed = 0
+        control_turn = 0
+
+        while not rospy.is_shutdown():
+            twist = self.calcTwist()
+            print(twist)
+            self.vel_pub.publish(twist)
+
+            r.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('random_run')
+    bot = RandomBot('Random', True)
+    bot.strategy()
+
